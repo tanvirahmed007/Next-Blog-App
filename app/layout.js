38814da417a07bf22a67d0ac71d94259b7cbae2b
tanvirahmed007 <thisistanvirmail@gmail.com>
2025-09-10 15:31:18 +0600

    Category,Newsletter,Footer added

--- app/layout.js
+++ app/layout.js
@@ -9,14 +9,15 @@ import Navbar from "./navbar/page";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"></link>
+      
       <body
         className="main-body"
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
-      <Footer />
+      
     </html>
   );
 }
