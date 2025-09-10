257d2a54c8a76d01f33e1c0f2e4f1c1d8adad937
tanvirahmed007 <thisistanvirmail@gmail.com>
2025-09-10 15:28:06 +0600

    Dynamic

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    domains: ['example.com', 'images.unsplash.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // or specify specific rules to ignore
+  },
+};
 
 export default nextConfig;
